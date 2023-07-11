b9b5c27383e7b0591a6359c8cb019476a9e14a02
Akinola <adewoleakinola24@gmail.com>
2023-07-11 23:37:43 +0100

    Class Component to Functonal Component

--- src/components/newComponent1/BukNav.jsx
+++ src/components/newComponent1/BukNav.jsx
@@ -3,45 +3,48 @@ import { ThemeContext } from '../../context/ThemeContext';
 import { AuthContext } from '../../context/AuthContextProvider';
 
 
-export default class BukNav extends Component {
-  render() {
+// export default class BukNav extends Component {
+//   render() {
    
-    return (
-      <>
-                  {/* To create multiple consumer */}
-              <AuthContext.Consumer>{(authContext)=>(
-                     <ThemeContext.Consumer>{(themeContext)=>{
-                              const{isAuthenticated, toggleAuth}= authContext
-                              const {isLightTheme, light, dark} = themeContext;
-                              const theme = isLightTheme ? light : dark
+//     return (
+//       <>
+//                   {/* To create multiple consumer */}
+//               <AuthContext.Consumer>{(authContext)=>(
+//                      <ThemeContext.Consumer>{(themeContext)=>{
+//                               const{isAuthenticated, toggleAuth}= authContext
+//                               const {isLightTheme, light, dark} = themeContext;
+//                               const theme = isLightTheme ? light : dark
               
-                              return (
+//                               return (
 
-                                <nav style={{background:theme.ui, color:theme.syntax}}>
-                                <h1>Context App</h1>
+//                                 <nav style={{background:theme.ui, color:theme.syntax}}>
+//                                 <h1>Context App</h1>
 
-                                    <div onClick={toggleAuth}> {isAuthenticated ? 'loggedIn' : 'loggedOut'} </div>
+//                                     <div onClick={toggleAuth}> {isAuthenticated ? 'loggedIn' : 'loggedOut'} </div>
 
-                                <ul>
-                                    <li>Home</li>
-                                    <li>Contact</li>
-                                    <li>About</li>
-                                </ul>
-                            </nav>
-                              )              
-                            }}
-                      </ThemeContext.Consumer>   
-              )}</AuthContext.Consumer>
+//                                 <ul>
+//                                     <li>Home</li>
+//                                     <li>Contact</li>
+//                                     <li>About</li>
+//                                 </ul>
+//                             </nav>
+//                               )              
+//                             }}
+//                       </ThemeContext.Consumer>   
+//               )}</AuthContext.Consumer>
 
-      </>
-    )
-  }
-}
+//       </>
+//     )
+//   }
+// }
 
-import React from 'react'
+
+// Functional Component
 
 const BukNav = () => {
-  const {isLightTheme, light, darkuseContext}=(ThemeContext)
+  const {isLightTheme, light, dark}= useContext(ThemeContext)
+  const{isAuthenticated, toggleAuth}= useContext(AuthContext)
+  const theme = isLightTheme ? light : dark
   return (
     <>
              <nav style={{background:theme.ui, color:theme.syntax}}>
