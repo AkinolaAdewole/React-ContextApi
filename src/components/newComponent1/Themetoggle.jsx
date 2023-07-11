99dae5c14e7735780c353408a4d2dd8e5ca1cf01
Akinola <adewoleakinola24@gmail.com>
2023-07-11 23:48:02 +0100

    Class Component to Functonal Component

--- src/components/newComponent1/Themetoggle.jsx
+++ src/components/newComponent1/Themetoggle.jsx
@@ -1,24 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component, useContext } from 'react'
 import { ThemeContext } from '../../context/ThemeContext'
 
-export default class Themetoggle extends Component {
-    static contextType=ThemeContext
+// export default class Themetoggle extends Component {
+//     static contextType=ThemeContext
    
-  render() {
-    const {toggleTheme}=this.context
-    return (
-      <div>
-        <button onClick={toggleTheme} >Toggle the Theme</button>
-      </div>
-    )
-  }
-}
+//   render() {
+//     const {toggleTheme}=this.context
+//     return (
+//       <div>
+//         <button onClick={toggleTheme} >Toggle the Theme</button>
+//       </div>
+//     )
+//   }
+// }
 
-import React from 'react'
+
 
 const Themetoggle = () => {
+    const {toggleTheme}= useContext(ThemeContext);
   return (
-    <>Themetoggle</>
+    <>
+            <div>   
+                  <button onClick={toggleTheme} >Toggle the Theme</button>
+            </div>
+    </>
   )
 }
 
