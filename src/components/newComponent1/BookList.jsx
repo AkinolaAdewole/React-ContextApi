f06164d942c74216dee68c7d669d42573c4f0d55
Akinola <adewoleakinola24@gmail.com>
2023-07-11 21:50:31 +0100

    useContext

--- src/components/newComponent1/BookList.jsx
+++ src/components/newComponent1/BookList.jsx
@@ -1,21 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component,useContext } from 'react'
 import { ThemeContext } from '../../context/ThemeContext'
 
-export default class BookList extends Component {
-    static contextType=ThemeContext
-  render() {
-    const {isLightTheme, light, dark}=this.context
-    const theme= isLightTheme ? light : dark;
-    return (
-      <>
-          <div className='book-list' style={{background:theme.bg, color:theme.syntax}}>
+// export default class BookList extends Component {
+//     static contextType=ThemeContext
+//   render() {
+//     const {isLightTheme, light, dark}=this.context
+//     const theme= isLightTheme ? light : dark;
+//     return (
+//       <>
+//           <div className='book-list' style={{background:theme.bg, color:theme.syntax}}>
+//             <ul>
+//                 <li style={{background:theme.ui}}>The legend of the lost legend</li>
+//                 <li style={{background:theme.ui}}>See you at the top</li>
+//                 <li style={{background:theme.ui}}>48 Laws of power</li>
+//             </ul>
+//           </div>
+//       </>
+//     )
+//   }
+// }
+
+
+import React from 'react'
+
+const BookList = () => {
+  return (
+    <>
+         <div className='book-list' style={{background:theme.bg, color:theme.syntax}}>
             <ul>
                 <li style={{background:theme.ui}}>The legend of the lost legend</li>
                 <li style={{background:theme.ui}}>See you at the top</li>
                 <li style={{background:theme.ui}}>48 Laws of power</li>
             </ul>
           </div>
-      </>
-    )
-  }
+    </>
+  )
 }
+
+export default BookList
